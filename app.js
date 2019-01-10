e5cc4b77a89c70d0d9f592086a07826ce9a68aeb
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-10 01:24:24 +0100

    finished banner

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ window.onscroll = function(){
 
 //sticky menu
 var stickyMenu = document.getElementsByClassName('stickyMenu')[0];
+var stickySocialMedia = document.getElementsByClassName('stickySocialMedia')[0];
 var stickySwitch = 0;
 function stickyMenuFunc(){
   if(this.oldScroll > this.scrollY && window.scrollY > 1 && stickySwitch===0){
     if(window.innerWidth > window.innerHeight){
         stickyMenu.style.height = '3.427vw';
+        stickySocialMedia.style.top = '0.893vw'
     }else{
       stickyMenu.style.height = '17vw';
     }
   }
   else {
     stickyMenu.style.height = '0vw';
+    stickySocialMedia.style.top = '-10vw'
   }
   this.oldScroll = this.scrollY;
 }
