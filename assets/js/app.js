c3ade3e5476268ef5dbb1baf2b34727a4ed4be0d
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-12 19:41:54 +0100

    third carousel complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,3 +91,34 @@ function carMoveTwo(){
     carouselTwo.style.left = '-40%'
   }
 }
+// carousel three
+// carousel three
+// carousel three
+var carouselThree = document.getElementById('carouselThree');
+var carThreeLeftArrow = document.getElementById('carThreeLeftArrow');
+var carThreeRightArrow = document.getElementById('carThreeRightArrow');
+carThreeLeftArrow.addEventListener('click',function(){
+  leftArrowThree();
+  console.log('do i work?');
+});
+carThreeRightArrow.addEventListener('click',function(){
+  rightArrowThree();
+  console.log('do i work?');
+});
+var moduloThree = 1002;
+function leftArrowThree(){
+  moduloThree--;
+  carMoveThree();
+}
+function rightArrowThree(){
+  moduloThree++;
+  carMoveThree();
+}
+function carMoveThree(){
+  if(moduloThree%2 === 0){
+    carouselThree.style.left = '0%'
+  }
+  if(moduloThree%2 === 1){
+    carouselThree.style.left = '-50%'
+  }
+}
