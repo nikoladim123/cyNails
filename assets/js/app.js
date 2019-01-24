3451d1a9f6383382fdff00ba9d9f4d5fefc2bebb
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-24 19:34:23 +0100

    car js

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,67 @@ function stickyMenuFunc(){
   this.oldScroll = this.scrollY;
 }
 
+// car bridal
+// car bridal
+// car bridal
+var bridalImage = document.getElementsByClassName('bridal');
+var bridalImageModulo = 0;
+var bridalHugeFont = document.getElementsByClassName('bridalHugeFont')[0];
+setInterval(function(){
+  bridalImageModulo++;
+  if(bridalImageModulo%4 === 3){
+    bridalHugeFont.style.opacity = 0;
+    setTimeout(function(){
+      bridalHugeFont.innerHTML = 'SKINCARE';
+      bridalHugeFont.style.opacity = 1;
+    },500)
+
+    bridalImage[0].style.opacity = '1'
+    bridalImage[1].style.opacity ='0';
+    bridalImage[2].style.opacity ='0';
+    bridalImage[3].style.opacity ='0';
+    console.log(bridalImageModulo%4 + " i am 0");
+  }
+  if(bridalImageModulo%4 === 1){
+    bridalHugeFont.style.opacity = 0;
+    setTimeout(function(){
+      bridalHugeFont.innerHTML = 'MANICURE';
+      bridalHugeFont.style.opacity = 1;
+    },500)
+
+    bridalImage[1].style.opacity ='1';
+    bridalImage[0].style.opacity ='0';
+    bridalImage[2].style.opacity ='0';
+    bridalImage[3].style.opacity ='0';
+    console.log(bridalImageModulo%4 + " i am 1");
+  }
+  if(bridalImageModulo%4 === 2){
+    bridalHugeFont.style.opacity = 0;
+    setTimeout(function(){
+      bridalHugeFont.innerHTML = 'PEDICURE';
+      bridalHugeFont.style.opacity = 1;
+    },500)
+
+    bridalImage[2].style.opacity ='1';
+    bridalImage[0].style.opacity ='0';
+    bridalImage[1].style.opacity ='0';
+    bridalImage[3].style.opacity ='0';
+    console.log(bridalImageModulo%4 + " i am 2");
+  }
+  if(bridalImageModulo%4 === 0){
+    bridalHugeFont.style.opacity = 0;
+    setTimeout(function(){
+      bridalHugeFont.innerHTML = 'BRIDAL';
+      bridalHugeFont.style.opacity = 1;
+    },500)
+
+    bridalImage[3].style.opacity ='1';
+    bridalImage[0].style.opacity ='0';
+    bridalImage[1].style.opacity ='0';
+    bridalImage[2].style.opacity ='0';
+    console.log(bridalImageModulo%4 + " i am 3");
+  }
+},3500);
 //carousel one
 //carousel one
 //carousel one
@@ -30,13 +91,25 @@ var carouselOne = document.getElementById('carouselOne');
 var carOneLeftCarArrowBox = document.getElementById('carOneLeftCarArrowBox');
 var carOneRightCarArrowBox = document.getElementById('carOneRightCarArrowBox');
 carOneLeftCarArrowBox.addEventListener('click',function(){
-  leftArrow();
+  if(window.innerWidth>window.innerHeight){
+    leftArrow();
+  }
+  if(window.innerWidth<window.innerHeight){
+    leftArrowCarOneMobile();
+  }
   console.log('do i work?');
 });
+
 carOneRightCarArrowBox.addEventListener('click',function(){
-  rightArrow();
+  if(window.innerWidth>window.innerHeight){
+    rightArrow();
+  }
+  if(window.innerWidth<window.innerHeight){
+    rightArrowCarOneMobile()
+  }
   console.log('do i work?');
 });
+/*car function for desktop*/
 var modulo = 1002;
 function leftArrow(){
   modulo--;
@@ -57,6 +130,60 @@ function carMove(){
     carouselOne.style.left = '-40%'
   }
 }
+/*car function for desktop end*/
+/*car one fun for mobile*/
+var mobileCarOneDiv = document.getElementsByClassName('mobileCarOneDiv');
+var dotsCarOne = document.getElementsByClassName('dotsCarOne');
+var carOneMobileModulo = 1000;
+
+function leftArrowCarOneMobile(){
+  carOneMobileModulo--;
+  for (var i = 0; i < mobileCarOneDiv.length-2; i++) {
+    mobileCarOneDiv[i].style.opacity ='0';
+    dotsCarOne[i].style.backgroundColor = 'white';
+    dotsCarOne[i].style.width = '3.5vw';
+    dotsCarOne[i].style.height = '3.5vw';
+}
+  setTimeout(function(){
+    for (var i = 0; i < mobileCarOneDiv.length-2; i++) {
+      mobileCarOneDiv[i].style.display ='none';
+    }
+    mobileCarOneDiv[carOneMobileModulo%5].style.display ='block';
+    mobileCarOneDiv[carOneMobileModulo%5].style.opacity ='0';
+      setTimeout(function(){
+        mobileCarOneDiv[carOneMobileModulo%5].style.opacity ='1';
+      },100);
+  },200);
+
+  dotsCarOne[carOneMobileModulo%5].style.backgroundColor = 'rgb(240,212,199)';
+  dotsCarOne[carOneMobileModulo%5].style.width = '4.5vw';
+  dotsCarOne[carOneMobileModulo%5].style.height = '4.5vw';
+}
+
+function rightArrowCarOneMobile(){
+  carOneMobileModulo++;
+  for (var i = 0; i < mobileCarOneDiv.length-2; i++) {
+    mobileCarOneDiv[i].style.opacity ='0';
+    dotsCarOne[i].style.backgroundColor = 'white';
+    dotsCarOne[i].style.width = '3.5vw';
+    dotsCarOne[i].style.height = '3.5vw';
+}
+  setTimeout(function(){
+    for (var i = 0; i < mobileCarOneDiv.length-2; i++) {
+      mobileCarOneDiv[i].style.display ='none';
+    }
+    mobileCarOneDiv[carOneMobileModulo%5].style.display ='block';
+    mobileCarOneDiv[carOneMobileModulo%5].style.opacity ='0';
+      setTimeout(function(){
+        mobileCarOneDiv[carOneMobileModulo%5].style.opacity ='1';
+      },100);
+  },200);
+
+  dotsCarOne[carOneMobileModulo%5].style.backgroundColor = 'rgb(240,212,199)';
+  dotsCarOne[carOneMobileModulo%5].style.width = '4.5vw';
+  dotsCarOne[carOneMobileModulo%5].style.height = '4.5vw';
+}
+/*car one fun for mobile end*/
 // carousel two
 // carousel two
 // carousel two
@@ -64,13 +191,27 @@ var carouselTwo = document.getElementById('carouselTwo');
 var carTwoLeftCarArrowBox = document.getElementById('carTwoLeftCarArrowBox');
 var carTwoRightCarArrowBox = document.getElementById('carTwoRightCarArrowBox');
 carTwoLeftCarArrowBox.addEventListener('click',function(){
-  leftArrowTwo();
-  console.log('do i work?');
+  if(window.innerWidth>window.innerHeight){
+    leftArrowTwo();
+  }
+  if(window.innerWidth<window.innerHeight){
+    leftArrowCarTwoMobile();
+  }
+
+  console.log('what car am i');
 });
+
 carTwoRightCarArrowBox.addEventListener('click',function(){
-  rightArrowTwo();
+  if(window.innerWidth>window.innerHeight){
+    rightArrowTwo();
+  }
+  if(window.innerWidth<window.innerHeight){
+    rightArrowCarTwoMobile();
+  }
+
   console.log('do i work?');
 });
+/*car two desktop*/
 var moduloTwo = 1002;
 function leftArrowTwo(){
   moduloTwo--;
@@ -91,6 +232,64 @@ function carMoveTwo(){
     carouselTwo.style.left = '-40%'
   }
 }
+/*car two desktop*/
+/*car two fun for mobile*/
+var mobileCarTwoDiv = document.getElementsByClassName('mobileCarTwoDiv');
+var dotsCarTwo = document.getElementsByClassName('dotsCarTwo');
+var carTwoMobileModulo = 1000;
+
+function leftArrowCarTwoMobile(){
+  carTwoMobileModulo--;
+  for (var i = 0; i < mobileCarTwoDiv.length-2; i++) {
+    mobileCarTwoDiv[i].style.opacity ='0';
+    dotsCarTwo[i].style.backgroundColor = 'white';
+    dotsCarTwo[i].style.width = '3.5vw';
+    dotsCarTwo[i].style.height = '3.5vw';
+  }
+  setTimeout(function(){
+    for (var i = 0; i < mobileCarTwoDiv.length-2; i++) {
+      mobileCarTwoDiv[i].style.display ='none';
+    }
+    mobileCarTwoDiv[carTwoMobileModulo%5].style.display ='block';
+    mobileCarTwoDiv[carTwoMobileModulo%5].style.opacity ='0';
+
+      setTimeout(function(){
+        mobileCarTwoDiv[carTwoMobileModulo%5].style.opacity ='1';
+      },100);
+  },200);
+  // mobileCarTwoDiv[carTwoMobileModulo%5].style.display ='block';
+  dotsCarTwo[carTwoMobileModulo%5].style.backgroundColor = 'rgb(240,212,199)';
+  dotsCarTwo[carTwoMobileModulo%5].style.width = '4.5vw';
+  dotsCarTwo[carTwoMobileModulo%5].style.height = '4.5vw';
+}
+
+function rightArrowCarTwoMobile(){
+  carTwoMobileModulo++;
+  for (var i = 0; i < mobileCarTwoDiv.length-2; i++) {
+    mobileCarTwoDiv[i].style.opacity ='0';
+    dotsCarTwo[i].style.backgroundColor = 'white';
+    dotsCarTwo[i].style.width = '3.5vw';
+    dotsCarTwo[i].style.height = '3.5vw';
+  }
+  setTimeout(function(){
+    for (var i = 0; i < mobileCarTwoDiv.length-2; i++) {
+      mobileCarTwoDiv[i].style.display ='none';
+    }
+    mobileCarTwoDiv[carTwoMobileModulo%5].style.display ='block';
+    mobileCarTwoDiv[carTwoMobileModulo%5].style.opacity ='0';
+
+      setTimeout(function(){
+        mobileCarTwoDiv[carTwoMobileModulo%5].style.opacity ='1';
+      },100);
+  },200);
+  // mobileCarTwoDiv[carTwoMobileModulo%5].style.display ='block';
+  dotsCarTwo[carTwoMobileModulo%5].style.backgroundColor = 'rgb(240,212,199)';
+  dotsCarTwo[carTwoMobileModulo%5].style.width = '4.5vw';
+  dotsCarTwo[carTwoMobileModulo%5].style.height = '4.5vw';
+}
+
+
+/*car two fun for mobile end*/
 // carousel three
 // carousel three
 // carousel three
@@ -122,3 +321,65 @@ function carMoveThree(){
     carouselThree.style.left = '-50%'
   }
 }
+// MOBILE CAROUSEL from grid box
+var mobileLeftArrow = document.getElementsByClassName('mobileLeftArrow')[0];
+var mobileRightArrow = document.getElementsByClassName('mobileRightArrow')[0];
+var gridPicturesJS = document.getElementsByClassName('gridPicturesJS');
+var dots = document.getElementsByClassName('dots');
+var mobileCarModulo = 71;
+
+mobileLeftArrow.addEventListener('click',function(){
+  mobileCarModulo--;
+  for (var i = 0; i < gridPicturesJS.length ; i++) {
+    gridPicturesJS[i].style.opacity = '0';
+    dots[i].style.backgroundColor = 'white';
+    dots[i].style.width = '3.5vw';
+    dots[i].style.height = '3.5vw';
+  }
+
+  setTimeout(function(){
+    for (var i = 0; i < gridPicturesJS.length-2; i++) {
+      gridPicturesJS[i].style.display ='none';
+    }
+    gridPicturesJS[mobileCarModulo%5].style.display ='block';
+    gridPicturesJS[mobileCarModulo%5].style.opacity ='0';
+
+      setTimeout(function(){
+        gridPicturesJS[mobileCarModulo%5].style.opacity ='1';
+      },100);
+  },200);
+
+  gridPicturesJS[mobileCarModulo%6].style.display = 'block';
+  dots[mobileCarModulo%6].style.backgroundColor = 'rgb(240,212,199)';
+  dots[mobileCarModulo%6].style.width = '4.5vw';
+  dots[mobileCarModulo%6].style.height = '4.5vw';
+  console.log(mobileCarModulo%7);
+})
+
+mobileRightArrow.addEventListener('click',function(){
+  mobileCarModulo++;
+  for (var i = 0; i < gridPicturesJS.length ; i++) {
+    gridPicturesJS[i].style.opacity = '0';
+    dots[i].style.backgroundColor = 'white';
+    dots[i].style.width = '3.5vw';
+    dots[i].style.height = '3.5vw';
+  }
+
+  setTimeout(function(){
+    for (var i = 0; i < gridPicturesJS.length-2; i++) {
+      gridPicturesJS[i].style.display ='none';
+    }
+    gridPicturesJS[mobileCarModulo%5].style.display ='block';
+    gridPicturesJS[mobileCarModulo%5].style.opacity ='0';
+
+      setTimeout(function(){
+        gridPicturesJS[mobileCarModulo%5].style.opacity ='1';
+      },100);
+  },200);
+
+  gridPicturesJS[mobileCarModulo%6].style.display = 'block';
+  dots[mobileCarModulo%5].style.backgroundColor = 'rgb(240,212,199)';
+  dots[mobileCarModulo%5].style.width = '4.5vw';
+  dots[mobileCarModulo%5].style.height = '4.5vw';
+  console.log(mobileCarModulo%7);
+})
